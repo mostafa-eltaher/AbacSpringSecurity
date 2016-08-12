c1747aedc2cfe0bb799867e25d7cead8e85aa354
Mostafa Eltaher <mostafa.mahmoud.eltaher@gmail.com>
2016-08-12 09:32:19 +0200

    Add AccessDenied REST handler

---
+++ src/main/java/com/mostafa/security/controllers/ExceptionControllerAdvice.java
@@ -0,0 +1,19 @@
+package com.mostafa.security.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice 
+public class ExceptionControllerAdvice {
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ResponseBody
+    public String exception(AccessDeniedException e) {
+        return "{\"status\":\"access denied\"}";
+    }
+}
